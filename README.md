b9b6cf9cf49455eb918876aa05591a6cabe2b7f2
nareshsharma123 <nareshsharma123@gmail.com>
2018-05-28 11:29:09 +0530

    Add live demo site

---
+++ README.md
@@ -0,0 +1,24 @@
+# YTSearch
+
+## What is this?
+This is a react application built using YouTube api.
+[Live Demo](https://devwrite.netlify.com/)
+
+## Usage
+
+This requires `YouTube Data API`.
+To acquire the said api Key navigate to https://console.developers.google.com/apis/dashboard
+
+```sh
+# install dependencies
+npm install
+
+# In ./src
+touch env.js
+```
+
+```sh
+# In env.js :
+const KEY = <your_key_here>
+export default KEY;
+```
---
+++ build/precache-manifest.b57b5507f4d5ce5fd3219188e44edd0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50ce4a5fb23a7402ac0c56d681e2fe08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d782b6faaead52e9b20b",
+    "url": "/static/css/main.600db6f8.chunk.css"
+  },
+  {
+    "revision": "b45edacf3d685dd12031",
+    "url": "/static/js/2.6783caff.chunk.js"
+  },
+  {
+    "revision": "d782b6faaead52e9b20b",
+    "url": "/static/js/main.942058a8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
